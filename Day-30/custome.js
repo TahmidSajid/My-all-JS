78467d5fe3df84a0aa731a009b9723040585d328
TahmidSajid <tahmidkhansajid@gmail.com>
2023-10-16 23:01:09 +0600

    dom start again

---
+++ Day-30/custome.js
@@ -0,0 +1,13 @@
+var myname = document.querySelector('input');
+let see_here = document.querySelector('.see_here');
+console.log(see_here);
+const changeHandeler = (e)=>{
+    console.log(e.target.value)
+    let text = e.target.value
+    see_here.innerHTML=text
+}
+myname.addEventListener("change",changeHandeler)
+
+// const changeHandeler = (e)=>{
+//     console.log('changed')
+// }
